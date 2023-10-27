00d9ee3d13d03479f7185e2230cc7e3bf18599ed
Dylanfelipe01 <dfelipe.puentes@gmail.com>
2023-10-27 08:46:23 -0500

    Dylan

---
+++ Assets/CODIGOS/MovimientoJugador.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoJugador : MonoBehaviour
+{
+    private Rigidbody2D rig;
+    private Animator anim;
+    private SpriteRenderer spritePersonaje;
+    [SerializeField] private float velocidadPersonaje;
+
+    private void Awake()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+        spritePersonaje = GetComponentInChildren<SpriteRenderer>();
+    }
+    private void FixedUpdate()
+    {
+        float velocidadInput = Input.GetAxisRaw("Horizontal");
+        rig.velocity = new Vector2(velocidadInput * velocidadPersonaje, rig.velocity.y);
+        anim.SetFloat("Camina", velocidadInput);
+
+        if (velocidadInput < 0)
+        {
+            spritePersonaje.flipX = true;
+        }
+        else if (velocidadInput > 0)
+        {
+            spritePersonaje.flipX = false;
+        }
+    }
+}
---
+++ Assets/CODIGOS/SaltoPersonaje.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaltoPersonaje : MonoBehaviour
+{
+    public Rigidbody2D rig;
+    public float fuerza;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))       
+        {
+            rig.AddForce(Vector2.up * fuerza, ForceMode2D.Impulse);
+        }
+    }
+}
